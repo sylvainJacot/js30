fd30f1d4d842331a0f82eaeb86b69cf0120119bb
coatriver <sylvain.jacot@hotmail.be>
2020-12-18 15:54:13 +0100

    first styling custom variables

---
+++ src/js/cursorvariables.js
@@ -0,0 +1,13 @@
+// To make Sass (or, specifically, SCSS in this case) variables available to JavaScript, we need to “export” them.
+// Then we import the Sass file file into JavaScript, giving us access to the variables defined in the file.
+// import variables from "../scss/01-config/variables.scss";
+
+const inputs = document.querySelectorAll("input");
+
+function handleUpdate() {
+  const suffix = this.dataset.unit;
+  console.log(suffix);
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdate));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdate));
